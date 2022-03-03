9299fbe714a169c6d1dfbc3807d37c7eedf293f7
mylliejohnson <mylliejohnson@gmail.com>
2022-03-03 18:59:11 -0500

    day 1

---
+++ 100DaysOfSwift/100DaysOfSwift.playground/Contents.swift
@@ -0,0 +1,31 @@
+// www.hackingwithswift.com/100
+
+
+// DAY 1 3/03 //
+
+var str = "HELLO, PLAYGROUND"
+str = "Goodbye"
+
+var age = 25
+var num = 8_000_000
+
+var example = """
+this goes
+over multiple
+lines
+"""
+
+var pi = 3.141
+var awesome = true
+
+var score = 85
+var str2 = "Your score was \(score)"
+var results = "The test results are here: \(str2)"
+
+let taylor = "swift" // let variables cannot be changed, use constants as often as possible
+
+let name: String = "Myllie"
+let myAge: Int = 25
+let girl: Bool = true
+let height: Double = 5.2
+
---
+++ App_100DaysOf/App_100DaysOf/App_100DaysOfApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_100DaysOfApp.swift
+//  App_100DaysOf
+//
+//  Created by Myllie Johnson on 3/3/22.
+//
+
+import SwiftUI
+
+@main
+struct App_100DaysOfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
