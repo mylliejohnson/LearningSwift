87d6ac14a16b2eb67783382903cc6b6d353f6a82
mylliejohnson <mylliejohnson@gmail.com>
2022-03-08 20:38:00 -0500

    day 6 closures

--- 100DaysOfSwift/100DaysOfSwift.playground/Contents.swift
+++ 100DaysOfSwift/100DaysOfSwift.playground/Contents.swift
@@ -384,3 +384,117 @@ func doubleInPlace(num: inout Int) { // inout variables can be changed inside fu
 
 var myNum = 5
 doubleInPlace(num: &myNum)
+
+
+
+
+
+// ----- DAY 6 3/08 CLOSURES ----- //
+
+// function that is assigned a variable and can pass into other functions
+
+let driving = {
+    print("I'm driving in my car")
+}
+
+driving()
+
+let driving2 = { (place: String) in
+    print("I'm going driving in \(place)")
+}
+
+driving2("London")
+
+let driving3 = { (place:String) -> String in
+    return "I'm going to \(place) in my car"
+}
+
+let message = driving3("Key West")
+print(message + "      --------- ")
+
+func travel(action: () -> Void){
+    print("I'm getting ready to go")
+    action()
+    print("I arrived!")
+}
+
+travel(action: driving)
+
+travel {
+    print("I'm driving in my car")
+}
+
+
+func travel2(action: (String) -> Void) {
+    print("I'm getting ready to go")
+    action("Coral Springs")
+    print("I arrived!")
+}
+
+travel2 {(place: String) in
+    print("I'm going to \(place) in my car")
+}
+
+func travel3(action: (String) -> String) {
+    print("I'm getting ready to go.")
+    let description = action("London")
+    print(description)
+    print("I've arrived!")
+}
+
+travel3 { (place:String) -> String in
+    return "I'm going to \(place) in my car"
+}
+
+travel3 { place in
+    "I'm going to \(place) in my car"
+}
+
+func travel4 (action: (String, Int) -> String) {
+    print("I'm getting ready to go.")
+    let description = action("London", 50)
+    print(description)
+    print("I've arrived!")
+}
+
+travel4 {
+    "I'm going to \($0) at \($1) miles per hour."
+}
+
+func travelling() -> (String) -> Void {
+    return {
+        print("I'm going to \($0)")
+    }
+}
+
+let res = travelling()
+res("Miami")
+
+let res2 = travelling()("Miami")
+
+
+func travel5() -> (String) -> Void {
+    return {
+        print("I'm going to \($0)")
+    }
+}
+
+let result5 = travel5()
+result5("London")
+
+func travel6() -> (String) -> Void {
+    var counter = 1
+
+    return {
+        print("\(counter). I'm going to \($0)")
+        counter += 1
+    }
+}
+ /* summary
+  1. You can assign closures to variables, then call them later on.
+  2. Closures can accept parameters and return values, like regular functions.
+  3. You can pass closures into functions as parameters, and those closures can have parameters of their own and a return value.
+  4. If the last parameter to your function is a closure, you can use trailing closure syntax.
+  5. Swift automatically provides shorthand parameter names like $0 and $1, but not everyone uses them.
+  6. If you use external values inside your closures, they will be captured so the closure can refer to them later.
+  */
